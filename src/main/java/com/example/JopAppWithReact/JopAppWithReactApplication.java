487a2c57af9b841c654cd04cfa6567d6d3c03a3b
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2026-01-07 00:07:15 +0530

    Initial commit

---
+++ src/main/java/com/example/JopAppWithReact/JopAppWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.JopAppWithReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopAppWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JopAppWithReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JopAppWithReact/JopAppWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JopAppWithReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JopAppWithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
