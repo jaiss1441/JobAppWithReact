f244356a6a25f158badb4414ce890594462e5d14
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2026-01-09 23:37:44 +0530

    CrossOrigin on Controller class

--- src/main/java/com/example/JopAppWithReact/JobController.java
+++ src/main/java/com/example/JopAppWithReact/JobController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin("")
+@CrossOrigin("localhost:3000")
 public class JobController {
 
     @Autowired
