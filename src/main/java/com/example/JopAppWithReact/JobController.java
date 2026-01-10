d0ac9d677e36e34a9680d40b6218052c93f0dda2
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2026-01-10 23:17:21 +0530

    CrossOrigin on Controller class

--- src/main/java/com/example/JopAppWithReact/JobController.java
+++ src/main/java/com/example/JopAppWithReact/JobController.java
@@ -3,10 +3,7 @@ package com.example.JopAppWithReact;
 import com.example.JopAppWithReact.model.JobPost;
 import com.example.JopAppWithReact.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,11 @@ public class JobController {
     @ResponseBody
     public List<JobPost> allJobPost(){
         return jobService.getallJobs();
+    }
+
+    @PostMapping
+    public String AddJob(){
+        return "New Job";
     }
 
 }
